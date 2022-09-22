6455302ed0550b4d9a33880733d69e9da41e38fe
Raze010 <54363525+Raze010@users.noreply.github.com>
2022-09-22 11:34:21 +0200

    1er bug résolu

--- resources/bdd/gsb_restore.sql
+++ resources/bdd/gsb_restore.sql
@@ -1,9 +1,9 @@
 -- Script de restauration de l'application "GSB Frais"
 
 -- Administration de la base de données
-CREATE DATABASE gsb_frais ;
-GRANT SHOW DATABASES ON *.* TO userGsb@localhost IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON `gsb_frais`.* TO userGsb@localhost;
+-- CREATE DATABASE gsb_frais;
+-- GRANT SHOW DATABASES ON *.* TO userGsb@localhost IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON `gsb_frais`.* TO rayane_Gsb@localhost;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 USE gsb_frais ;
 
