6a78f3189e3732ff8584e608831a7d434b81c3c9
Ancestra11 <AdrienD.pro@yahoo.com>
2022-10-16 02:11:51 -0700

    Retour du dossier vendor car son absence cause des erreurs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gsb/applifrais',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '94d865f68331049592db7fb76566432c2b6b9a8e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gsb/applifrais' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '94d865f68331049592db7fb76566432c2b6b9a8e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
