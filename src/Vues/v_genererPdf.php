1361d236c7b2e5e42ffd44122350d6e970ee8ad2
Raze010 <54363525+Raze010@users.noreply.github.com>
2022-10-18 11:44:38 +0200

    Pdf ajout de titre et de rect

--- src/Vues/v_genererPdf.php
+++ src/Vues/v_genererPdf.php
@@ -24,5 +24,19 @@ $pdf_xSize =$pdf->GetPageWidth();
 $xpos = $pdf_xSize / 2 - $img_size[0] / 2 * 0.75;
 $pdf->Image($img,$xpos ,20);
 
+//RECT_TITRE
+$marge = 50;
+$rect_size = $pdf_xSize - $marge * 2;
+$pdf ->Rect($marge, 200, $rect_size, 30);
+
+//TITRE
+$pdf ->Text($marge + 10, 220, "REMBOURSEMENT DE FRAIS ENGAGES");
+
+//RECT_CORPS
+$pdf -> Rect($marge,230,$rect_size,300);
+
+//Presentation visiteur
+$pdf -> text($marge * 2,230 + $marge,"Visiteur          NRD/A-131           Louis VILLECHALANE");
+
 $pdf->Output();
 
