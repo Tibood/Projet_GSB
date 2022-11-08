d3a6cb6db53bf1717871338ee26a1570303b4da5
Tibo <95851801+Tibood@users.noreply.github.com>
2022-11-08 08:29:47 +0100

    Update avec la 2°solution

--- src/Modeles/ajoutComptable.sql
+++ src/Modeles/ajoutComptable.sql
@@ -1,6 +1,38 @@
 USE gsb_frais;
-DROP TABLE IF EXISTS COMPTABLE;
 
+Drop table if exists metiers;
+create Table metiers (
+    IdVisiteur VARCHAR(20) NOT NULL,
+    IdMetier int not null,
+    constraint PK_ROLES primary key (IdVisiteur,IdMetier),
+    Constraint FK_Roles_Metier foreign key (idMetier)
+        references metier(id),
+    Constraint FK_roles_visiteur foreign key (IdVisiteur)
+        references Visiteur(id)
+);
+
+Drop table if exists roles;
+create table roles (
+    id int not null auto_increment,
+    libelle varchar (30) not null,
+    constraint PK_Metier primary key (id)
+);
+
+insert into roles value (1,'Comptable');
+insert into metiers value ('f4',1);
+insert into metiers value ('f39',1);
+
+
+Select visiteur.nom,visiteur.prenom
+from visiteur inner join metiers
+on visiteur.id = metiers.IdVisiteur
+where metiers.IdMetier = 1;
+
+
+
+/*
+Solution n°1
+DROP TABLE IF EXISTS COMPTABLE;
 CREATE TABLE IF NOT EXISTS COMPTABLE (
     LIBELLE VARCHAR(50) NOT NULL,
     IDVISITEUR CHAR(4) NOT NULL,
@@ -16,3 +48,7 @@ SELECT Visiteur.mdp FROM Visiteur
 INNER JOIN COMPTABLE 
 ON COMPTABLE.IDVISITEUR = Visiteur.id
 WHERE COMPTABLE.LIBELLE = 'Comptable';
+*/
+
+
+
