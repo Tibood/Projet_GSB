344b151dd89dd29bf4bad4f71c93b38b07daa6da
Tibo <tib.brouillet@gmail.com>
2022-11-15 18:14:24 +0100

    ajout variables aux inputs

--- src/Vues/v_validerFicheFrais.php
+++ src/Vues/v_validerFicheFrais.php
@@ -18,23 +18,24 @@
         <h2>Eléments forfaitisé</h2>
         <label>Fofait Etape</label>
         <br/>
-        <input type="text" id="name" name="name" required
+        <input type="text" value='<?php $fEtape = 2?>' id="Fofait_Etape" name="Fofait_Etape" required
             minlength="4" maxlength="8" size="10">
         <br/>
         <label>Frais Kilometrique</label>
         <br/>
-        <input type="text" id="name" name="name" required
+        <input type="text" value='<?php $fKilometrique = 2?>' id="Frais_Kilometrique" name="Frais_Kilometrique" required
             minlength="4" maxlength="8" size="10">
         <br/>
         <label>Repas Restaurant</label>
         <br/>
-        <input type="text" id="name" name="name" required
+        <input type="text" value='<?php $repResto = 2?>' id="Repas_Restaurant" name="Repas_Restaurant" required
             minlength="4" maxlength="8" size="10">
     </div>
     <br/>
     <input type="button" value="Corriger"class="btn btn-success"></input>
     <input type="button" value="Reinitialiser"class="btn btn-danger"></input>
 </div>
+<br/>
 <div class='panel panel-info'>
     <div class="panel-heading">Descriptif des éléments hors forfait</div>
     <table class='table table-bordered table-responsive'>
