45a33f1a1935480bde3dae6e78b8aff915309d8c
Tibo <tib.brouillet@gmail.com>
2022-12-02 23:58:19 +0100

    identation et ajout valeur par default

--- src/Vues/v_validerFicheFrais.php
+++ src/Vues/v_validerFicheFrais.php
@@ -43,7 +43,6 @@
         <label for="listMois">Mois :</label>
         <select class="form-control" name="listMois" id="listMois">
             <?php
-            /*
             foreach ($lesMois as $unMois) {
                 $mois = $unMois['mois'];
                 $numAnnee = $unMois['numAnnee'];
@@ -59,64 +58,61 @@
                         <?php echo $numMois . '/' . $numAnnee ?> </option>
                     <?php
                 }
-                }
-                ?>
-                */
-                ?>
+            }
+            ?>
         </select>
         </div>
     </form>
 </div>
 <br>
+
+
 <!--AFFICHAGE-->
 <div class='container'>
-<h1>Valider la fiche de frais</h1>
-
-    <div class="input-group" id="retour">
-    </div>
-
-
+    <h1>Valider la fiche de frais</h1>
     <div class="input-group">
         <h2>Eléments forfaitisé</h2>
         <label>Fofait Etape</label>
         <br/>
-        <input type="text" value='<?php $fEtape = 2?>' id="Fofait_Etape" name="Fofait_Etape" required
+        <input type="text" value='<?php echo $fraisForfait[0]['quantite']?>' id="Fofait_Etape" name="Fofait_Etape" required
             minlength="4" maxlength="8" size="10">
         <br/>
         <label>Frais Kilometrique</label>
         <br/>
-        <input type="text" value='<?php $fKilometrique = 2?>' id="Frais_Kilometrique" name="Frais_Kilometrique" required
+        <input type="text" value='<?php echo $fraisForfait[1]['quantite']?>' id="Frais_Kilometrique" name="Frais_Kilometrique" required
             minlength="4" maxlength="8" size="10">
         <br/>
         <label>Repas Restaurant</label>
         <br/>
-        <input type="text" value='<?php $repResto = 2?>' id="Repas_Restaurant" name="Repas_Restaurant" required
+        <input type="text" value='<?php echo $fraisForfait[3]['quantite'] ?>' id="Repas_Restaurant" name="Repas_Restaurant" required
             minlength="4" maxlength="8" size="10">
     </div>
     <br/>
     <input type="button" value="Corriger"class="btn btn-success"></input>
     <input type="button" value="Reinitialiser"class="btn btn-danger"></input>
-</div>
-<br/>
-<div class='panel panel-info'>
-    <div class="panel-heading">Descriptif des éléments hors forfait</div>
-    <table class='table table-bordered table-responsive'>
-        <tbody>
-            <tr>
-                <th>Date</th>
-                <th>Libellé</th>
-                <th>Montant</th>
-                <th></th>
-            </tr>
-            <tr>
-                <td><div id="date"></div>test</td>
-                <td><div id="Libelle"></div>Cell4</td>
-                <td><div id="Montant"></div>Cell4</td>
-                <td>
-                <button type="button" class="btn btn-success">Corriger</button>
-                <button type="button" class="btn btn-danger">Reinitialiser</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <br/>
+    <br/>
+    <br/>
+    <div class='panel panel-info'>
+        <div class="panel-heading">Descriptif des éléments hors forfait</div>
+        <table class='table table-bordered table-responsive'>
+            <tbody>
+                <tr>
+                    <th>Date</th>
+                    <th>Libellé</th>
+                    <th>Montant</th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <td><div id="date"></div>test</td>
+                    <td><div id="Libelle"></div>Cell4</td>
+                    <td><div id="Montant"></div>Cell4</td>
+                    <td>
+                    <button type="button" class="btn btn-success">Corriger</button>
+                    <button type="button" class="btn btn-danger">Reinitialiser</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
