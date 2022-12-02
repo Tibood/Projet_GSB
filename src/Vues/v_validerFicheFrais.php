1755cdf9cf799b150f0f6dce62ec0d0bb334c328
Tibo <tib.brouillet@gmail.com>
2022-12-03 00:38:09 +0100

    ajout element manquant plus typage des input

--- src/Vues/v_validerFicheFrais.php
+++ src/Vues/v_validerFicheFrais.php
@@ -16,7 +16,6 @@
 
 <!--SELECTION-->
 <div>
-    <br>
         <label for ='listVisiteur'>Choisir le visiteur:</label>
         <select class="form-control" name="listVisiteur" id='listVisiteur' onchange="getMois(this.value);" >
             <?php
@@ -64,28 +63,25 @@
         </div>
     </form>
 </div>
-<br>
+
 
 
 <!--AFFICHAGE-->
 <div class='container'>
     <h1>Valider la fiche de frais</h1>
     <div class="input-group">
-        <h2>Eléments forfaitisé</h2>
+        <h3>Eléments forfaitisé</h3>
         <label>Fofait Etape</label>
         <br/>
-        <input type="text" value='<?php echo $fraisForfait[0]['quantite']?>' id="Fofait_Etape" name="Fofait_Etape" required
-            minlength="4" maxlength="8" size="10">
+        <input type="number" value='<?php echo $fraisForfait[0]['quantite']?>' id="Fofait_Etape" name="Fofait_Etape" required>
         <br/>
         <label>Frais Kilometrique</label>
         <br/>
-        <input type="text" value='<?php echo $fraisForfait[1]['quantite']?>' id="Frais_Kilometrique" name="Frais_Kilometrique" required
-            minlength="4" maxlength="8" size="10">
+        <input type="number" value='<?php echo $fraisForfait[1]['quantite']?>' id="Frais_Kilometrique" name="Frais_Kilometrique" required>
         <br/>
         <label>Repas Restaurant</label>
         <br/>
-        <input type="text" value='<?php echo $fraisForfait[3]['quantite'] ?>' id="Repas_Restaurant" name="Repas_Restaurant" required
-            minlength="4" maxlength="8" size="10">
+        <input type="number" value='<?php echo $fraisForfait[3]['quantite'] ?>' id="Repas_Restaurant" name="Repas_Restaurant" required>
     </div>
     <br/>
     <input type="button" value="Corriger"class="btn btn-success"></input>
@@ -103,16 +99,38 @@
                     <th>Montant</th>
                     <th></th>
                 </tr>
-                <tr>
-                    <td><div id="date"></div>test</td>
-                    <td><div id="Libelle"></div>Cell4</td>
-                    <td><div id="Montant"></div>Cell4</td>
-                    <td>
-                    <button type="button" class="btn btn-success">Corriger</button>
-                    <button type="button" class="btn btn-danger">Reinitialiser</button>
-                    </td>
+                    <?php
+                    foreach ($fraisHorsForfait as $unFraisHorsForfait) {
+                        ?>
+                        <tr>
+                            <td>
+                                <input type="text" value='<?php echo $unFraisHorsForfait['date'] ?>'
+                                        name="date" required>
+                            </td>
+                            <td>
+                                <input type="text" value='<?php echo $unFraisHorsForfait['libelle'] ?>'
+                                        name="libelle" required>
+                            </td>
+                            <td>
+                                <input type="number" value='<?php echo $unFraisHorsForfait['montant'] ?>'
+                                        name="montant" required>
+                            </td>
+                            <td>
+                                <input type="button" value="Corriger" class="btn btn-success"></input>
+                                <input type="button" value="Reinitialiser" class="btn btn-danger"></input>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
                 </tr>
             </tbody>
         </table>
     </div>
+    <label for="Nb_justificatif">Nombre de justificatifs :</label>
+    <input type="number" value='<?php echo $nbJustificatif?>' id="Nb_justificatif" name="Nb_justificatif" required>
+    <br/>
+    <br/>
+    <input type="button" value="Valider" class="btn btn-success"></input>
+    <input type="button" value="Reinitialiser" class="btn btn-danger"></input>
 </div>
