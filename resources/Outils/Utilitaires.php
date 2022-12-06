95008af25ad5fcaeb6a7f8c0490a245f5a8c2c73
Adrien DODERO <adrien.dodero@bts.sio>
2022-12-06 11:54:02 +0100

    Meilleur doc bloc

--- resources/Outils/Utilitaires.php
+++ resources/Outils/Utilitaires.php
@@ -51,9 +51,11 @@ abstract class Utilitaires
     /**
      * Utilise la librairie de SendinBlue pour envoyer un mail
      * pour la double authentification, depuis le serveur de production.
-     * Il est nécessaire de spécifier le chemin d'un fichier .pem (clef privé) 
-     * pour que ça fonctionne. Et corriger la dépréciation de SendinBlue envers PHP 8.1
-     * (Spécifier le type de retour des fonctions dans 2 fichiers dans le dossier vendor)
+     * Il est nécessaire de spécifier le chemin d'un fichier .pem (clef privé) dans le fichier
+     * php.ini d'Apache pour que ça fonctionne. Et corriger la dépréciation de SendinBlue 
+     * envers PHP 8.1(Spécifier le type de retour des fonctions dans 2 fichiers 
+     * dans le dossier vendor/sendinblue/api-v3-sdk/lib/Model/CreateSmtpEmail[Sender].php
+     * (plus d'infos dans le fichier tests/txt/Enleve-Deprecation-Sendinblue.txt))
      * 
      * @return null
      */ 
