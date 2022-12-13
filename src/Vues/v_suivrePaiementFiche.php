cc0cdee50ffb8888a3b7b15b4807d2faa0524aa4
Tibo <tib.brouillet@gmail.com>
2022-12-13 22:27:59 +0100

    Update v_suivrePaiementFiche.php

---
+++ src/Vues/v_suivrePaiementFiche.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Vue suivre paiement des Fiche de Frais
+ *
+ * PHP Version 8
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    BROUILLET THIBAUD
+ */
+?>
+
+<script src = "assets/js/ValiderFicheFrais.js"></script>
+<script src = "//code.jquery.com/jquery-1.12.0.min.js"></script>
+<link href="styles/styleComptable.css" rel="stylesheet" type="text/css">
+
+<!--SELECTION-->
+<span class="badge badge-primary">Connecté sur le compte de <?php echo($_SESSION['prenom']." ". $_SESSION['nom'])?></span>
+<br>
+<br>
