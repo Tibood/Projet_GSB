55add4fd3cd1cfdf5c3313785526bda4f67576b9
rayane bosso <RBOSSOR@gmail.com>
2022-12-14 19:02:13 +0100

    Revert "ajout du script sql pour la tache 6"
    This reverts commit 3b21d32863eb5ff47380bb28d4d4b5dfc41a99e2.

--- resources/bdd/Tache6Bdd.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `gsb_frais`.`visiteur` 
-ADD COLUMN `typeVehicule` VARCHAR(45) NOT NULL DEFAULT '4CV Diesel' COMMENT 'Le type du vehicule qui influencent le frais kilomètrique\n' AFTER `email`;
