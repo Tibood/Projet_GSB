a76eed69f66695eae84b3c3386ec4beaee1221ad
Tibo <tib.brouillet@gmail.com>
2022-12-14 23:12:53 +0100

    supp espace en trop

--- resources/bdd/Vehicule.sql
+++ resources/bdd/Vehicule.sql
@@ -1,7 +1,7 @@
 use gsb_frais;
 
 drop table if exists vehicule;
-create table if not exists vehicule 
+create table if not exists vehicule
 (
 	id int not null auto_increment,
     libelle varchar(50) null,
@@ -15,8 +15,8 @@ insert into vehicule (id,libelle,prixaukilometre) Values
 (3,"Véhicule 4CV Essence",0.62),
 (4,"Véhicule 5/6CV Essence",0.67);
 
-alter table visiteur 
-add idvehicule int null, 
+alter table visiteur
+add idvehicule int null,
 add constraint FK_visiteur_vehicule FOREIGN KEY (idvehicule) REFERENCES vehicule(id) on delete cascade;
 
 SET SQL_SAFE_UPDATES = 0;
